5f64a5ef754b9a90dd772ef22e6db9633c7fd258
dev-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-11-30 12:23:13 +0600

    code updated

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oc06hsd.mongodb.net/?retryWrites=true&w=majority`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
